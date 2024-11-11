8484b472c769c147ba326cef0310cf38d4fbf996
kophe <lorenzo.bosi.pro@gmail.com>
2024-11-11 15:54:44 +0100

    add background - set default zoom and position - add corner blur (a revoir plus tard dans les detail)

---
+++ src/app/components/Layout.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Layout: React.FC = ({ children }) => {
+  return (
+    <div className="relative w-screen h-screen overflow-hidden">
+      {/* Contenu de la scène Three.js */}
+        <div className="absolute inset-0">
+            {children}
+        </div>
+
+        {/* <div className='relative h-screen w-screen overflow-hidden'> */}
+
+            <div className='absolute -top-40 -left-40 w-80 blur-3xl h-80 rounded-full bg-slate-900/20 '></div>
+            <div className='absolute -top-40 -right-40 w-80 blur-3xl h-80 rounded-full bg-slate-900/20 '></div>
+            <div className='absolute -bottom-40 -left-40 w-80 blur-3xl h-80 rounded-full bg-slate-900/20 '></div>
+            <div className='absolute -bottom-40 -right-40 w-80 blur-3xl h-80 rounded-full bg-slate-900/20'></div>
+        {/* </div> */}
+
+
+        {/* Point central */}
+        {/* <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-2 h-2 bg-black rounded-full z-10" /> */}
+
+        {/* Ligne horizontale */}
+        {/* <div className="absolute top-1/2 left-0 w-full h-px bg-black z-5" /> */}
+
+        {/* Ligne verticale */}
+        {/* <div className="absolute top-0 left-1/2 w-px h-full bg-black z-5" /> */}
+    </div>
+  );
+};
+
+export default Layout;
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 // import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader';
 import { RGBELoader } from "three/examples/jsm/Addons.js";
+import Layout from "./components/Layout";
 
 export default function Home() {
   const mountRef = useRef<HTMLDivElement>(null);
@@ -19,12 +20,12 @@ export default function Home() {
     if (isClient && mountRef.current) {
       const scene = new THREE.Scene();
       const camera = new THREE.PerspectiveCamera(
-        45,
+        35,
         window.innerWidth / window.innerHeight,
         0.1,
         1000
       );
-      camera.position.set(0, 0.5, 4);
+      camera.position.set(0, 0.2, 4);
 
       const renderer = new THREE.WebGLRenderer({ antialias: true });
       renderer.setSize(window.innerWidth, window.innerHeight);
@@ -45,7 +46,7 @@ export default function Home() {
       const loader = new GLTFLoader().setPath('/3dmodel/olympus_film_camera_pbr_scan_project3/');
       loader.load('scene.gltf', (gltf) => {
         const mesh = gltf.scene;
-        mesh.position.set(0, -0.5, 0);
+        mesh.position.set(0.05, -0.8, 0);
         mesh.traverse((node) => {
           if (node.isMesh) {
             node.castShadow = true;
@@ -63,21 +64,11 @@ export default function Home() {
       controls.maxDistance = 3;
       controls.maxPolarAngle = Math.PI / 2;
 
-      // Sol
-      // const groundGeometry = new THREE.PlaneGeometry(20, 20);
-      // groundGeometry.rotateX(-Math.PI / 2);
-      // const groundMaterial = new THREE.MeshStandardMaterial({
-      //   color: 0x555555,
-      //   roughness: 0.8,
-      //   metalness: 0.2
-      // });
-      // const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
-      // groundMesh.receiveShadow = true;
-      // scene.add(groundMesh);
-
       // Lumière ambiante
       const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
       scene.add(ambientLight);
+
+      scene.background = new THREE.Color(0x504644);
 
       // Lumière directionnelle
       const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
@@ -121,7 +112,9 @@ export default function Home() {
   }, [isClient]);
 
   return (
-    <div ref={mountRef} style={{ width: "100vw", height: "100vh" }} />
+    <Layout>
+      <div ref={mountRef} style={{ width: "100vw", height: "100vh" }} />
+    </Layout>
   );
 }
 
